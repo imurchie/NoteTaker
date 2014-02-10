e405e15ecec37ce5af07b14cb76a2a99ae669b1f
Isaac Murchie <isaac@saucelabs.com>
2014-02-09 18:54:26 -0800

    Add tab bar

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,7 +4,15 @@ class AppDelegate
     @window.makeKeyAndVisible
 
     note_controller = NoteViewController.alloc.init
-    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(note_controller)
+    nav_controller  = UINavigationController.alloc.initWithRootViewController(note_controller)
+
+    other_controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
+    other_controller.title = "Other"
+    other_controller.view.backgroundColor = UIColor.purpleColor
+
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller.viewControllers = [nav_controller, other_controller]
+    @window.rootViewController = tab_controller
 
     true
   end
--- app/controllers/note_view_controller.rb
+++ app/controllers/note_view_controller.rb
@@ -11,6 +11,20 @@ class NoteViewController < UIViewController
     @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
     self.view.addSubview @label
 
-    self.title = "NoteTaker"
+    self.title = "NoteTaker (#{self.navigationController.viewControllers.count})"
+
+    right_button = UIBarButtonItem.alloc.initWithTitle("Push", style: UIBarButtonItemStyleBordered, target:self, action:'push')
+    self.navigationItem.rightBarButtonItem = right_button
+  end
+
+  def push
+    new_controller = NoteViewController.alloc.initWithNibName(nil, bundle: nil)
+    self.navigationController.pushViewController(new_controller, animated: true)
+  end
+
+  def initWithNibName(name, bundle: bundle)
+    super
+    self.tabBarItem = UITabBarItem.alloc.initWithTabBarSystemItem(UITabBarSystemItemFavorites, tag: 1)
+    self
   end
 end
